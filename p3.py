6090064be876c1c58f94d57d55851b69a2ad25fe
bhasv <bhasvitakatragadda@gmail.com>
2025-04-05 11:58:39 +0530

    finding factorial of a number

--- p3.py
+++ p3.py
@@ -1,5 +1,5 @@
-num = int(input("Enter the number to find the factorial: "))
+n = int(input("Enter the number to find the factorial: "))
 factorial = 1
-for i in range(1, num + 1):
+for i in range(1, n + 1):
     factorial *= i
-print(f"The factorial of {num} is {factorial}")
+print(f"The factorial of {n} is {factorial}")
